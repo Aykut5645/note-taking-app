567f0bc6f10ecfc657f34c25ba5707e8aef64001
Aykut5645 <aykut.fatin@abv.bg>
2024-06-01 14:42:17 +0300

    Enhance the functionality of Details page

--- src/features/NoteItem.tsx
+++ src/features/NoteItem.tsx
@@ -1,16 +1,20 @@
-import {Card, Col} from "antd";
+import {Card, Col } from "antd";
+import {Link} from "react-router-dom";
 
 type NoteItemProps = {
   id: string;
   title: string;
-  description: string;
+  description?: string;
 };
 
 const NoteItem = ({ id, title, description }: NoteItemProps) => {
   return (
     <Col style={{ marginBottom: 16 }} key={id} xs={24} md={12} xl={8}>
-      <Card style={{ height: '100%' }} title={title} bordered={false} extra={<a href="#">More</a>}>
+      <Card style={{ height: '100%' }} title={title} bordered={false}
+            extra={<Link to={`/note/${id}`}>More</Link>}
+      >
         {description}
+
       </Card>
     </Col>
   );
--- src/pages/NoteDetails.tsx
+++ src/pages/NoteDetails.tsx
@@ -1,5 +1,33 @@
+import {Button, Divider, Flex, Space} from "antd";
+import {useParams} from "react-router-dom";
+import {useState} from "react";
+
 const NoteDetails = () => {
-  return <div>Note Details</div>;
+  const { id } = useParams();
+  const [note] = useState<{id:string;title:string;description?:string;}>(() => {
+    const notes = localStorage.getItem('notes');
+    if (notes) {
+      return JSON.parse(notes).find((note:{id:string;title:string;description?:string;}) => note.id === id);
+    }
+  });
+
+  return (
+    <>
+      <h2>Your Note</h2>
+      <Divider/>
+      <div>
+        <Space direction="vertical" size="middle">
+          <h3>{note.title}</h3>
+          <p>{note.description}</p>
+        </Space>
+      </div>
+      <Divider/>
+      <Flex gap="small" wrap justify="flex-end">
+        <Button type="primary">Edit</Button>
+        <Button danger>Delete</Button>
+      </Flex>
+    </>
+  );
 };
 
 export default NoteDetails;
