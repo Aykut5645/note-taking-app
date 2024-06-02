3541171fe27c56fdc65b3cf7360ad41530695497
Aykut5645 <aykut.fatin@abv.bg>
2024-06-02 19:26:52 +0300

    Created reusable NoteDetailsFooter.tsx

---
+++ src/components/NoteDetailsFooter.tsx
@@ -0,0 +1,44 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Divider, Flex, Modal } from 'antd';
+import { ExclamationCircleFilled } from '@ant-design/icons';
+
+import { useNotes } from '../hooks/useNotes.tsx';
+const { confirm } = Modal;
+
+const showDeleteConfirm = (handler: () => void) => {
+  confirm({
+    title: 'Delete Note?',
+    content: 'Are you sure you want to delete this note?',
+    icon: <ExclamationCircleFilled />,
+    okType: 'danger',
+    okText: 'Yes',
+    cancelText: 'No',
+    onOk: handler,
+  });
+};
+
+const NoteDetailsFooter = ({ id }: { id: string }) => {
+  const navigate = useNavigate();
+  const notesCtx = useNotes();
+
+  const handleDeleteNote = () => {
+    notesCtx.deleteNote(id!);
+    navigate('/');
+  };
+
+  return (
+    <>
+      <Divider />
+      <Flex gap="small" wrap>
+        <Link to={`/edit-note/${id}`}>
+          <Button type="primary">Edit</Button>
+        </Link>
+        <Button danger onClick={() => showDeleteConfirm(handleDeleteNote)}>
+          Delete
+        </Button>
+      </Flex>
+    </>
+  );
+};
+
+export default NoteDetailsFooter;
--- src/pages/NoteDetails.tsx
+++ src/pages/NoteDetails.tsx
@@ -1,36 +1,16 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, Divider, Flex, Space, Modal } from 'antd';
-import { ExclamationCircleFilled } from '@ant-design/icons';
+import { useParams } from 'react-router-dom';
+import { Button, Space } from 'antd';
 
 import { useNotes } from '../hooks/useNotes.tsx';
+import { useMoveBack } from '../hooks/useMoveBack.tsx';
 import Title from '../ui/Title.tsx';
 import CustomResult from '../ui/CustomResult.tsx';
-import { useMoveBack } from '../hooks/useMoveBack.tsx';
-
-const { confirm } = Modal;
-
-const showDeleteConfirm = (handler: () => void) => {
-  confirm({
-    title: 'Delete Note?',
-    content: 'Are you sure you want to delete this note?',
-    icon: <ExclamationCircleFilled />,
-    okType: 'danger',
-    okText: 'Yes',
-    cancelText: 'No',
-    onOk: handler,
-  });
-};
+import NoteDetailsFooter from '../components/NoteDetailsFooter.tsx';
 
 const NoteDetails = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
   const notesCtx = useNotes();
   const moveBack = useMoveBack();
-
-  const handleDeleteNote = () => {
-    notesCtx.deleteNote(id!);
-    navigate('/');
-  };
 
   const currentNote = notesCtx.notes.find((note) => note.id === id);
 
@@ -51,21 +31,11 @@ const NoteDetails = () => {
   return (
     <>
       <Title>Your Note</Title>
-      <div>
-        <Space direction="vertical" size="middle">
-          <h3>{currentNote.title}</h3>
-          <p>{currentNote.description}</p>
-        </Space>
-      </div>
-      <Divider />
-      <Flex gap="small" wrap>
-        <Link to={`/edit-note/${id}`}>
-          <Button type="primary">Edit</Button>
-        </Link>
-        <Button onClick={() => showDeleteConfirm(handleDeleteNote)} danger>
-          Delete
-        </Button>
-      </Flex>
+      <Space direction="vertical" size="middle">
+        <h3>{currentNote.title}</h3>
+        <p>{currentNote.description}</p>
+      </Space>
+      <NoteDetailsFooter id={id!} />
     </>
   );
 };
